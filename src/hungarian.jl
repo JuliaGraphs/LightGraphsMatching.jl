ed488d74898805fce68617a8bf6323d37251edb9
Thibaut Cuvelier <dourouc05@gmail.com>
2018-11-07 00:13:11 +0100

    Solve deprecations.

--- src/hungarian.jl
+++ src/hungarian.jl
@@ -15,12 +15,11 @@ function maximum_weight_maximal_matching_hungarian(g::Graph,
   to_bipartition_2 = [count(bipartition[1:i] .== 2) for i in 1:n]
 
   # hungarian() minimises the total cost, while this function is supposed to maximise the total weights.
-  wDual = maximum(w) - w
+  wDual = maximum(w) .- w
 
   # Remove weights that are not in the graph (Hungarian.jl considers all weights that are not missing values as real edges).
   # Assume w is symmetric, so that the weight of matching i->j is the same as the one for j->i. 
-  weights = Matrix{Union{Missing, T}}(n_first, n_second)
-  fill!(weights, missing)
+  weights = Matrix{Union{Missing, T}}(missing, n_first, n_second)
 
   for i in 1:n
     for j in 1:n
@@ -48,8 +47,8 @@ function maximum_weight_maximal_matching_hungarian(g::Graph,
   mate = fill(-1, n) # Initialise to unmatched. 
   for i in eachindex(assignment)
     if assignment[i] != 0 # If matched: 
-      original_i = find(to_bipartition_1 .== i)[1]
-      original_j = find(to_bipartition_2 .== assignment[i])[1]
+      original_i = findfirst(to_bipartition_1 .== i)
+      original_j = findfirst(to_bipartition_2 .== assignment[i])
 
       mate[original_i] = original_j
       mate[original_j] = original_i
